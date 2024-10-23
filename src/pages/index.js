082529cccb8b4727a4195dd03daa92b9c7748673
Eric Dahl <edahl@nd.edu>
2024-10-23 16:11:53 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 // pages/index.js
 import client from '../lib/redisClient';
+import styles from '../styles/Home.module.css'; // Importing CSS module for styling
 
 export default function Home({ wordleAnswer }) {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Today&#39;s Wordle Answer is...</h1>
-      <p style={{ fontSize: '24px' }}>{wordleAnswer}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Today&#39;s Wordle Answer is...</h1>
+      <p className={styles.answer}>{wordleAnswer}</p>
     </div>
   );
 }
 
 export async function getServerSideProps() {
   try {
-    // Replace 'wordle:today' with your actual Redis key
     const wordleAnswer = await client.get('wordle:today');
-
     return {
       props: {
         wordleAnswer: wordleAnswer || 'No answer available',
