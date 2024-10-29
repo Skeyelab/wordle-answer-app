fc95e84d38c4acd13851253aad9968265a5c0221
Eric Dahl <edahl@nd.edu>
2024-10-29 15:34:51 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import client from '../lib/redisClient';
 const Home = ({ wordleAnswer }) => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
-      <h1 className="text-4xl font-bold text-white mb-4">Today&#39;s Wordle Answer is...</h1>
-      <p className="text-6xl font-extrabold text-yellow-300">{wordleAnswer}</p>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Today&#39;s Wordle Answer is...</h1>
+      <p className="text-6xl font-extrabold text-blue-600">{wordleAnswer}</p>
     </div>
   );
 };
