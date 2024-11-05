d2cbcd816773fd2dba259cfb4de5133cb3ab6ec0
Eric Dahl <edahl@nd.edu>
2024-11-05 13:38:45 -0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <script defer src="https://umami.apps.stereorail.com/script.js" data-website-id="a9c38a73-fd3a-47fc-b6c1-7db381adb94a"></script>
       </body>
     </Html>
   );
